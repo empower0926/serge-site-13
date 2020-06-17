0dbea72144ab8163072be04be0285bf46f32d841
Buddhi96 <Buddhika Madhuranga>
2020-06-17 07:30:13 +0530

    created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import User from './user_pannels/user';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <User/>
+    </div>
+  );
+}
+
+export default App;
